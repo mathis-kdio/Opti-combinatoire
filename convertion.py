63918293ef212999a3d0fa0927f8e79d948f17aa
bastjog <bastienjoguet@gmail.com>
2021-10-14 16:55:39 +0200

    glouton fonctionnel
    fonctionnel mais pas opti de fou

--- convertion.py
+++ convertion.py
@@ -41,8 +41,6 @@ if __name__ == '__main__':
     convives[m[j][0]][2].append(m[j][1])
     convives[m[j][1]][2].append(m[j][0])
 
-  #res = glouton(convives)
-  #print("res: " + str(res))
 
   # Ecriture du résultat dans le fichier lp
   result = []
@@ -80,3 +78,7 @@ if __name__ == '__main__':
   for mot in result:
       fichierSortie.write(str(mot) + "\n")
   fichierSortie.close()
+
+  
+  res = glouton(convives)
+  print("res: " + str(res))
