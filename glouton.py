d2ebf108de95933c7b5b9a922e7af42480557cf3
bastjog <bastienjoguet@gmail.com>
2021-10-16 12:12:36 +0200

    changement nom variable pour rapport

--- glouton.py
+++ glouton.py
@@ -6,7 +6,7 @@ def glouton(convives, randomize):
     tmp = 0
     theOne = 0
     solution = []
-    listVoisin = []
+    listConnaissance = []
     connaissancesH = []
 
     if randomize == False:
@@ -16,32 +16,32 @@ def glouton(convives, randomize):
             if heuristique > tmp:
                 tmp = heuristique
                 theOne = i
-
         solution.append(theOne)
-        listVoisin.append(list(convives[theOne][2]))
+        listConnaissance.append(list(convives[theOne][2]))
+
     if randomize == True:
         theOne = random.randrange(0, len(convives))
         solution.append(theOne)
-        listVoisin.append(list(convives[theOne][2]))
+        listConnaissance.append(list(convives[theOne][2]))
 
     #On trie le tableau des voisins en fonction de la meilleure heuristique
     for i in range (0, len(convives[theOne][2])):
-        h = convives[listVoisin[0][i]][1] * len(convives[listVoisin[0][i]][2])
-        connaissancesH.append([h, listVoisin[0][i]])
+        h = convives[listConnaissance[0][i]][1] * len(convives[listConnaissance[0][i]][2])
+        connaissancesH.append([h, listConnaissance[0][i]])
     
     connaissancesH.sort(reverse=True)
     for i in range(0, len(connaissancesH)):
-        listVoisin[0][i] = connaissancesH[i][1]
+        listConnaissance[0][i] = connaissancesH[i][1]
 
-    while listVoisin[0] != []:
+    while listConnaissance[0] != []:
         apparition = 0
-        for j in range(0, len(listVoisin)):
-            if listVoisin[0][0] in listVoisin[j]:
+        for j in range(0, len(listConnaissance)):
+            if listConnaissance[0][0] in listConnaissance[j]:
                 apparition += 1 
-        if apparition == len(listVoisin):
-            listVoisin.append(list(convives[listVoisin[0][0]][2]))
-            solution.append(listVoisin[0][0])
+        if apparition == len(listConnaissance):
+            listConnaissance.append(list(convives[listConnaissance[0][0]][2]))
+            solution.append(listConnaissance[0][0])
                             
-        listVoisin[0].pop(0)
+        listConnaissance[0].pop(0)
 
     return solution
