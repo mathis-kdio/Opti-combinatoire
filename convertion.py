bcf99ac8ae1b4cf7b11eecd41e333e5b4ae328a6
Paul MARRET <39690393+pmarret@users.noreply.github.com>
2021-10-16 19:46:49 +0200

    tentative taboue

--- convertion.py
+++ convertion.py
@@ -4,100 +4,98 @@ from genetique import *
 
 if __name__ == '__main__':
 
-  Tab_result_GLPK = [73, 91, 84, 83, 81, 80, 81, 85, 77, 91]
-  argu = sys.argv[1]
-  sans_instance = argu.split("instance")
-  sans_txt = sans_instance[1].split(".txt")
-  instance_num = sans_txt[0]
+    Tab_result_GLPK = [73, 91, 84, 83, 81, 80, 81, 85, 77, 91]
+    argu = sys.argv[1]
+    sans_instance = argu.split("instance")
+    sans_txt = sans_instance[1].split(".txt")
+    instance_num = sans_txt[0]
 
-  print("Projet Optimisation Combinatoire")
+    print("Projet Optimisation Combinatoire")
 
-  # Lecture du fichier d'instance
-  try:
-      fichier = open(sys.argv[1], "r")
-  except:
-      sys.exit("Impossible de lire le fichier")
-  
-  lignes = fichier.readlines()
-  fichier.close()
+    # Lecture du fichier d'instance
+    try:
+        fichier = open(sys.argv[1], "r")
+    except:
+        sys.exit("Impossible de lire le fichier")
 
-  # Parsing
-  nm = lignes[0].split(" ")
-  nbConvives = int(nm[0])
-  m = int(nm[0])
-  lignes.pop(0)
-  print("nombre de convives potentiels: " + str(nbConvives))
-  print("nombre de liens d’amitié: " + str(m))
+    lignes = fichier.readlines()
+    fichier.close()
 
-  m = []
-  for ligne in lignes:
-    elements = ligne.split("\n")
-    for element in elements:
-      if (element != ""):
-        m.append([int(x) for x in element.split(' ')])
+    # Parsing
+    nm = lignes[0].split(" ")
+    nbConvives = int(nm[0])
+    m = int(nm[0])
+    lignes.pop(0)
+    print("nombre de convives potentiels: " + str(nbConvives))
+    print("nombre de liens d’amitié: " + str(m))
 
-  # Fin parsing
+    m = []
+    for ligne in lignes:
+        elements = ligne.split("\n")
+        for element in elements:
+            if (element != ""):
+                m.append([int(x) for x in element.split(' ')])
 
-  convives = []
-  for i in range(0, nbConvives):
-    convives.append(m[i])
-    convives[i].append([])
+    # Fin parsing
 
-  for j in range(nbConvives, len(m)):
-    convives[m[j][0]][2].append(m[j][1])
-    convives[m[j][1]][2].append(m[j][0])
+    convives = []
+    for i in range(0, nbConvives):
+        convives.append(m[i])
+        convives[i].append([])
 
+    for j in range(nbConvives, len(m)):
+        convives[m[j][0]][2].append(m[j][1])
+        convives[m[j][1]][2].append(m[j][0])
 
-  # Ecriture du résultat dans le fichier lp
-  result = []
-  result.append("Maximize")
-  
-  objectif = "z:"
-  for i in range(0, nbConvives):
-    objectif += " " + str(convives[i][1]) + " x" + str(i) + " +"
-  objectif = objectif[:len(objectif)-1]
-  result.append(objectif)
+    # Ecriture du résultat dans le fichier lp
+    result = []
+    result.append("Maximize")
 
-  result.append("Subject To")
+    objectif = "z:"
+    for i in range(0, nbConvives):
+        objectif += " " + str(convives[i][1]) + " x" + str(i) + " +"
+    objectif = objectif[:len(objectif) - 1]
+    result.append(objectif)
 
-  k = 0
-  for i in range(0, nbConvives):
-    for j in range(i + 1, nbConvives):
-      if j not in convives[i][2]:
-        poids = "c" + str(k) + ":"
-        poids += " x" + str(i) + " + x" + str(j) + " <= 1"
-        k += 1
-        result.append(poids)
+    result.append("Subject To")
 
-  result.append("Binaries")
+    k = 0
+    for i in range(0, nbConvives):
+        for j in range(i + 1, nbConvives):
+            if j not in convives[i][2]:
+                poids = "c" + str(k) + ":"
+                poids += " x" + str(i) + " + x" + str(j) + " <= 1"
+                k += 1
+                result.append(poids)
 
-  for i in range(0, nbConvives):
-    result.append("x" + str(i))
+    result.append("Binaries")
 
-  result.append("End")
+    for i in range(0, nbConvives):
+        result.append("x" + str(i))
 
-  try:
-      fichierSortie = open(sys.argv[2], "w")
-  except:
-      sys.exit("Impossible d'écrire dans le fichier")
+    result.append("End")
 
-  for mot in result:
-      fichierSortie.write(str(mot) + "\n")
-  fichierSortie.close()
+    try:
+        fichierSortie = open(sys.argv[2], "w")
+    except:
+        sys.exit("Impossible d'écrire dans le fichier")
 
-  solution = []
+    for mot in result:
+        fichierSortie.write(str(mot) + "\n")
+    fichierSortie.close()
 
-  population = initPop(convives, 200)
-  for i in range(0, len(population)):
-    solution.append(population[i][:])
-  population = selection(convives, population, 100)
-  population = croisement(population, 0.8)
-  population = mutation(population, 2)
-  population = reparation(convives, population)
-  for i in range(0, len(population)):
-    solution.append(population[i][:])
-  survie(convives, solution, 200)
-  print(solution)
+    solution = []
 
-  
+    population = initPop(convives, 200)
+    for i in range(0, len(population)):
+        solution.append(population[i][:])
+    population = selection(convives, population, 100)
+    population = croisement(population, 0.8)
+    population = mutation(population, 2)
+    population = reparation(convives, population)
+    for i in range(0, len(population)):
+        solution.append(population[i][:])
+    survie(convives, solution, 200)
+    print(solution)
 
+    solution_taboue = liste_taboue(convives, 1000)
--- glouton.py
+++ glouton.py
@@ -1,7 +1,8 @@
 import random
 
+
 def glouton(convives, randomize):
-    #Déclaration des variables
+    # Déclaration des variables
     heuristique = 0
     tmp = 0
     theOne = 0
@@ -10,7 +11,7 @@ def glouton(convives, randomize):
     connaissancesH = []
 
     if randomize == False:
-        #On détermine la meilleure heuristique avec le critère de choix et on ajoute sa valeur ainsi que ses voisins
+        # On détermine la meilleure heuristique avec le critère de choix et on ajoute sa valeur ainsi que ses voisins
         for i in range(0, len(convives)):
             heuristique = convives[i][1] * len(convives[i][2])
             if heuristique > tmp:
@@ -24,11 +25,11 @@ def glouton(convives, randomize):
         solution.append(theOne)
         listConnaissance.append(list(convives[theOne][2]))
 
-    #On trie le tableau des voisins en fonction de la meilleure heuristique
-    for i in range (0, len(convives[theOne][2])):
+    # On trie le tableau des voisins en fonction de la meilleure heuristique
+    for i in range(0, len(convives[theOne][2])):
         h = convives[listConnaissance[0][i]][1] * len(convives[listConnaissance[0][i]][2])
         connaissancesH.append([h, listConnaissance[0][i]])
-    
+
     connaissancesH.sort(reverse=True)
     for i in range(0, len(connaissancesH)):
         listConnaissance[0][i] = connaissancesH[i][1]
@@ -37,11 +38,41 @@ def glouton(convives, randomize):
         apparition = 0
         for j in range(0, len(listConnaissance)):
             if listConnaissance[0][0] in listConnaissance[j]:
-                apparition += 1 
+                apparition += 1
         if apparition == len(listConnaissance):
             listConnaissance.append(list(convives[listConnaissance[0][0]][2]))
             solution.append(listConnaissance[0][0])
-                            
+
         listConnaissance[0].pop(0)
 
     return solution
+
+
+def liste_taboue(convives, iteration):
+    print("liste taboue")
+
+    liste_voisin = []
+    liste_heuristique = []
+    heuristique = 0
+    connard = 0
+
+    for i in range(iteration):
+        res_glouton = glouton(convives, True)
+        liste_voisin.append(res_glouton)
+
+    for j in liste_voisin:
+        heuristique = 0
+        for k in j:
+            heuristique += convives[k][1]
+        liste_heuristique.append(heuristique)
+
+    print('liste heuristique', liste_heuristique)
+    liste_heuristique.append(heuristique)
+    index_liste_heur = liste_heuristique.index(max(liste_heuristique))
+
+    print('liste max', liste_voisin[index_liste_heur])
+
+    for l in liste_voisin[index_liste_heur]:
+        connard += convives[l][1]
+
+    print('connard', connard)
