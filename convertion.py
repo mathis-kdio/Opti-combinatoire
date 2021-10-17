7b123608465a9d40a98f26ad3a39f48568872986
bastjog <bastienjoguet@gmail.com>
2021-10-17 12:10:06 +0200

    sortie de parsing et writeLp du main

--- convertion.py
+++ convertion.py
@@ -1,14 +1,16 @@
 import sys
 
 from genetique import *
+from parsing import parsing, writeLp
 
 if __name__ == '__main__':
 
-  Tab_result_GLPK = [73, 91, 84, 83, 81, 80, 81, 85, 77, 91]
   argu = sys.argv[1]
-  sans_instance = argu.split("instance")
-  sans_txt = sans_instance[1].split(".txt")
-  instance_num = sans_txt[0]
+  if argu.split("instance"):
+    tab_result_GLPK = [73, 91, 84, 83, 81, 80, 81, 85, 77, 91]
+    sans_instance = argu.split("instance")
+    sans_txt = sans_instance[1].split(".txt")
+    instance_num = sans_txt[0]
 
   print("Projet Optimisation Combinatoire")
 
@@ -22,21 +24,7 @@ if __name__ == '__main__':
   fichier.close()
 
   # Parsing
-  nm = lignes[0].split(" ")
-  nbConvives = int(nm[0])
-  m = int(nm[0])
-  lignes.pop(0)
-  print("nombre de convives potentiels: " + str(nbConvives))
-  print("nombre de liens d’amitié: " + str(m))
-
-  m = []
-  for ligne in lignes:
-    elements = ligne.split("\n")
-    for element in elements:
-      if (element != ""):
-        m.append([int(x) for x in element.split(' ')])
-
-  # Fin parsing
+  nbConvives, m = parsing(lignes)
 
   convives = []
   for i in range(0, nbConvives):
@@ -47,44 +35,12 @@ if __name__ == '__main__':
     convives[m[j][0]][2].append(m[j][1])
     convives[m[j][1]][2].append(m[j][0])
 
-
   # Ecriture du résultat dans le fichier lp
-  result = []
-  result.append("Maximize")
-  
-  objectif = "z:"
-  for i in range(0, nbConvives):
-    objectif += " " + str(convives[i][1]) + " x" + str(i) + " +"
-  objectif = objectif[:len(objectif)-1]
-  result.append(objectif)
+  if argu.split("instance"):
+    writeLp(convives, nbConvives)
 
-  result.append("Subject To")
 
-  k = 0
-  for i in range(0, nbConvives):
-    for j in range(i + 1, nbConvives):
-      if j not in convives[i][2]:
-        poids = "c" + str(k) + ":"
-        poids += " x" + str(i) + " + x" + str(j) + " <= 1"
-        k += 1
-        result.append(poids)
-
-  result.append("Binaries")
-
-  for i in range(0, nbConvives):
-    result.append("x" + str(i))
-
-  result.append("End")
-
-  try:
-      fichierSortie = open(sys.argv[2], "w")
-  except:
-      sys.exit("Impossible d'écrire dans le fichier")
-
-  for mot in result:
-      fichierSortie.write(str(mot) + "\n")
-  fichierSortie.close()
-
+  # Algo génétique
   solution = []
   taillePop = 400
   tailleS = 100
---
+++ parsing.py
@@ -0,0 +1,56 @@
+import sys
+
+def parsing(lignes):
+    nm = lignes[0].split(" ")
+    nbConvives = int(nm[0])
+    m = int(nm[0])
+    lignes.pop(0)
+    print("nombre de convives potentiels: " + str(nbConvives))
+    print("nombre de liens d’amitié: " + str(m))
+
+    m = []
+    for ligne in lignes:
+        elements = ligne.split("\n")
+        for element in elements:
+            if (element != ""):
+                m.append([int(x) for x in element.split(' ')])
+
+    return nbConvives, m
+
+
+def writeLp(convives, nbConvives):
+    result = []
+    result.append("Maximize")
+    
+    objectif = "z:"
+    for i in range(0, nbConvives):
+        objectif += " " + str(convives[i][1]) + " x" + str(i) + " +"
+    objectif = objectif[:len(objectif)-1]
+    result.append(objectif)
+
+    result.append("Subject To")
+
+    k = 0
+    for i in range(0, nbConvives):
+        for j in range(i + 1, nbConvives):
+            if j not in convives[i][2]:
+                poids = "c" + str(k) + ":"
+                poids += " x" + str(i) + " + x" + str(j) + " <= 1"
+                k += 1
+                result.append(poids)
+
+    result.append("Binaries")
+
+    for i in range(0, nbConvives):
+        result.append("x" + str(i))
+
+    result.append("End")
+
+    try:
+        fichierSortie = open(sys.argv[2], "w")
+    except:
+        sys.exit("Impossible d'écrire dans le fichier")
+
+    for mot in result:
+        fichierSortie.write(str(mot) + "\n")
+    fichierSortie.close()
