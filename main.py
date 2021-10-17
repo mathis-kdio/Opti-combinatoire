0fc08ac9828bb54ba1456b0d94b9b35a41ae1e36
mathis kdio <cadio@et.esiea.fr>
2021-10-17 20:57:47 +0200

    Update main

--- main.py
+++ main.py
@@ -2,24 +2,16 @@ import sys
 
 from genetique import *
 from parsing import parsing
-from glouton import *
 
 if __name__ == '__main__':
-
-  argu = sys.argv[1]
-  if argu.split("instance"):
-    tab_result_GLPK = [73, 91, 84, 83, 81, 80, 81, 85, 77, 91]
-    sans_instance = argu.split("instance")
-    sans_txt = sans_instance[1].split(".txt")
-    instance_num = sans_txt[0]
-
   print("Projet Optimisation Combinatoire")
 
   # Lecture du fichier d'instance
+  inputFile = sys.argv[2]
   try:
-      fichier = open(sys.argv[1], "r")
+    fichier = open(inputFile, "r")
   except:
-      sys.exit("Impossible de lire le fichier")
+    sys.exit("Impossible de lire le fichier")
   
   lignes = fichier.readlines()
   fichier.close()
@@ -36,24 +28,28 @@ if __name__ == '__main__':
     convives[m[j][0]][2].append(m[j][1])
     convives[m[j][1]][2].append(m[j][0])
 
-  # Ecriture du résultat dans le fichier lp
-  #if argu.split("instance"):
-  #  writeLp(convives, nbConvives)
-
+  #Ecriture dans le fichier .lp
+  #ouputLPFile = sys.argv[3]
+  #writeLp(convives, nbConvives, ouputLPFile)
 
   # Algo génétique
+  '''
   solution = []
   taillePop = 400
   tailleS = 100
   pc = 0.8
   pm = 2
   iterMax = 500
+  tempsMax = int(sys.argv[1])
+  (interet, solution) = genetique(convives, pc, pm, taillePop, tailleS, iterMax, tempsMax)
 
+  outputFile = sys.argv[3]
+  try:
+    fichierSortie = open(outputFile, "w")
+  except:
+    sys.exit("Impossible d'écrire dans le fichier")
+  fichierSortie.write("Interet maximum trouve : " + str(interet) + " avec les " + str(len(solution)) + " invites suivants : " + str(solution) + "\n")
+  fichierSortie.close()
+  '''
   res_tabu = tabu_search(convives, 10)
   print("res_tabu ", res_tabu)
-
-  #best = genetique(convives, pc, pm, taillePop, tailleS, iterMax)
-
-  #f = open("output", "a")
-  #f.write("instance: " + sys.argv[1] + ' resultat : ' + str(best) + "\n")
-  #f.close()
